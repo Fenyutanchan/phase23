414f1ee7f27213943d69cf521b0a28cc2c6d9492
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2024-11-27 22:44:31 +0800

    update run.sh

--- wolfram_scripts/s100_m1/run.sh
+++ wolfram_scripts/s100_m1/run.sh
@@ -1,40 +1,40 @@
 echo "Start Time: $(date +%H:%M:%S)"
-wolframscript -f phase1.wls > phase1.log &
-wolframscript -f phase2.wls > phase2.log &
-wolframscript -f phase3.wls > phase3.log &
-wolframscript -f phase4.wls > phase4.log &
-wolframscript -f phase5.wls > phase5.log &
-wolframscript -f phase6.wls > phase6.log &
-wolframscript -f phase7.wls > phase7.log &
-wolframscript -f phase8.wls > phase8.log &
+WolframKernel -script phase1.wls > phase1.log &
+WolframKernel -script phase2.wls > phase2.log &
+WolframKernel -script phase3.wls > phase3.log &
+WolframKernel -script phase4.wls > phase4.log &
+WolframKernel -script phase5.wls > phase5.log &
+WolframKernel -script phase6.wls > phase6.log &
+WolframKernel -script phase7.wls > phase7.log &
+WolframKernel -script phase8.wls > phase8.log &
 wait
-wolframscript -f phase9.wls > phase9.log &
-wolframscript -f phase10.wls > phase10.log &
-wolframscript -f phase11.wls > phase11.log &
-wolframscript -f phase12.wls > phase12.log &
-wolframscript -f phase13.wls > phase13.log &
-wolframscript -f phase14.wls > phase14.log &
-wolframscript -f phase15.wls > phase15.log &
-wolframscript -f phase16.wls > phase16.log &
+WolframKernel -script phase9.wls > phase9.log &
+WolframKernel -script phase10.wls > phase10.log &
+WolframKernel -script phase11.wls > phase11.log &
+WolframKernel -script phase12.wls > phase12.log &
+WolframKernel -script phase13.wls > phase13.log &
+WolframKernel -script phase14.wls > phase14.log &
+WolframKernel -script phase15.wls > phase15.log &
+WolframKernel -script phase16.wls > phase16.log &
 wait
-wolframscript -f phase17.wls > phase17.log &
-wolframscript -f phase18.wls > phase18.log &
-wolframscript -f phase19.wls > phase19.log &
-wolframscript -f phase20.wls > phase20.log &
-wolframscript -f phase21.wls > phase21.log &
-wolframscript -f phase22.wls > phase22.log &
-wolframscript -f phase23.wls > phase23.log &
-wolframscript -f phase24.wls > phase24.log &
+WolframKernel -script phase17.wls > phase17.log &
+WolframKernel -script phase18.wls > phase18.log &
+WolframKernel -script phase19.wls > phase19.log &
+WolframKernel -script phase20.wls > phase20.log &
+WolframKernel -script phase21.wls > phase21.log &
+WolframKernel -script phase22.wls > phase22.log &
+WolframKernel -script phase23.wls > phase23.log &
+WolframKernel -script phase24.wls > phase24.log &
 wait
-wolframscript -f phase25.wls > phase25.log &
-wolframscript -f phase26.wls > phase26.log &
-wolframscript -f phase27.wls > phase27.log &
-wolframscript -f phase28.wls > phase28.log &
-wolframscript -f phase29.wls > phase29.log &
-wolframscript -f phase30.wls > phase30.log &
-wolframscript -f phase31.wls > phase31.log &
-wolframscript -f phase32.wls > phase32.log &
+WolframKernel -script phase25.wls > phase25.log &
+WolframKernel -script phase26.wls > phase26.log &
+WolframKernel -script phase27.wls > phase27.log &
+WolframKernel -script phase28.wls > phase28.log &
+WolframKernel -script phase29.wls > phase29.log &
+WolframKernel -script phase30.wls > phase30.log &
+WolframKernel -script phase31.wls > phase31.log &
+WolframKernel -script phase32.wls > phase32.log &
 wait
-wolframscript -f phase33.wls > phase33.log &
+WolframKernel -script phase33.wls > phase33.log &
 wait
 echo "End Time: $(date +%H:%M:%S)"
